b32c536ed1337cac862d8de2a06c10c5cd5dc8c6
karrassphiri <phi21020@byui.edu>
2022-02-05 20:30:41 +0200

    new chamber folder added

---
+++ lesson5/bom.js
@@ -0,0 +1,10 @@
+const listsize = document.querySelector('#favcap');
+const buildVutton = document.querySelector('button');
+const list = document.querySelector('#list')
+
+button.addEventListener('click', () => {
+  let myItem = input.value;
+  input.value = '';
+
+  listItem.appendChild(list)
+});
