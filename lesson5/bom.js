35df5162f9d03aa806a2534e75b052394ce70f3a
karrassphiri <phi21020@byui.edu>
2022-02-08 22:30:08 +0200

    small view placeholders done

--- lesson5/bom.js
+++ lesson5/bom.js
@@ -1,10 +1,11 @@
-const listsize = document.querySelector('#favcap');
-const buildVutton = document.querySelector('button');
+const listsize = document.querySelector('#listsize');
+const buildButton = document.querySelector('button');
 const list = document.querySelector('#list')
 
-button.addEventListener('click', () => {
-  let myItem = input.value;
+buildButton.addEventListener('click', () => {
+  let list = input.value;
   input.value = '';
-
+  let li =document.createElement('li')
+  li.innerHTML = `Item: $`
   listItem.appendChild(list)
 });
