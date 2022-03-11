4c38b1a81c8fd076547812cf602aff81dd7c911b
karrassphiri <phi21020@byui.edu>
2022-03-11 15:08:33 +0200

    push to the dev branch

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -10,10 +10,55 @@
 // long, medium, short options ... try them
 
 //datefield.innerHTML = `<em>${fulldate}</em>`;
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+//const hambutton = document.querySelector('.ham');
+//const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+//hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+//window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+/*** Programming Notes **************************************
+     Arrow Functions - es6 syntactically compact alternative to a regular function expression
+     see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+     or https://www.w3schools.com/js/js_arrow_function.asp
+   
+     classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+     */
+
+const datefieldUK = document.getElementById("date-time"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+var date = new Date();
+var year = date.getUTCFullYear();
+document.querySelector("#copyrightyear").innerHTML = year;
+
+
+
+document.querySelector(
+  "#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
